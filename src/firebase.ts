7374175a8590b6999ec18efd83ebbeceef76a14a
Jonghwa <whdghk3471@gmail.com>
2025-04-20 16:24:51 +0900

    #2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByyXs6rHrhemoshpEO4S0AfXxJAatNhqs",
+  authDomain: "nwitter-reloaded-d919a.firebaseapp.com",
+  projectId: "nwitter-reloaded-d919a",
+  storageBucket: "nwitter-reloaded-d919a.firebasestorage.app",
+  messagingSenderId: "351525718967",
+  appId: "1:351525718967:web:4f422590723536d4113632",
+};
+
+const app = initializeApp(firebaseConfig);
